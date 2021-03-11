2bceeeea404948549d5ef703b0f739ab0dd55300
Sheng.Xu <949525088@qq.com>
2021-03-11 16:19:23 +0800

    v1
    v1

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import state from '@x/state'
+import getters from '@x/getters'
 import mutations from '@x/mutations'
 import actions from '@x/actions'
 import user from '@x/modules/user-module'
@@ -15,6 +16,7 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   strict: true,
   state,
+  getters,
   mutations,
   actions,
   modules: {
--- src/store/modules/user-module.js
+++ src/store/modules/user-module.js
@@ -3,6 +3,7 @@ const mutations = {}
 const actions = {}
 
 export default {
+  namespaced: true,
   state,
   mutations,
   actions
