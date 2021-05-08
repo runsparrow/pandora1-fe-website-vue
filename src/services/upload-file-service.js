39143c8def4652ea637ca6a154cf6445c15bfa00
Sheng.Xu <949525088@qq.com>
2021-05-08 10:29:01 +0800

    v1
    v1

--- src/services/upload-file-service.js
+++ src/services/upload-file-service.js
@@ -4,5 +4,5 @@ import ajaxFile from '@l/ajax-file-interceptor'
 import store from '@x'
 
 export const uploadFileService = async payload => {
-  return ajaxFile.post(process.env.VUE_APP_BASE_API_URL + CONFIG.API_URLS.Upload_API_URL, payload)
+  return ajaxFile.post(process.env.VUE_APP_FILE_BASE_API_URL + CONFIG.API_URLS.Upload_API_URL, payload)
 }
